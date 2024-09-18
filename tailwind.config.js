41f9f59bf858c05aaa34c6de298de731e264c754
Kevin Martinez <kevinmartinez130605@gmail.com>
2024-09-18 13:48:11 -0500

    Estructured completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       boxShadow:{
         'custom-box': '0px 0px 15px 	rgb(0, 0, 0, .2)'
       },
+      backgroundImage:{
+        'footer-texture' : "url('img/6674f0cdb5b7b401612cf065_noise-50.jpg')"
+      },
     },
   },
   
